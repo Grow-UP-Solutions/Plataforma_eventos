9d8841316d9ccbc14825c59a2fbad24d0de9ce7d
guillobr <guillermobr88@gmail.com>
2022-09-12 13:23:59 -0300

    api

---
+++ client/src/api/categories.js
@@ -0,0 +1,90 @@
+const categories= [
+    {
+        name:'Al aire libre',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Artes',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Autosuperacion',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Belleza',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Ciencia',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Comidas y Bebidas',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Deportes',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Educacion(no formal)',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Finanzas',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name: 'Nuevos Amigos',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Idiomas',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name: 'Para niños',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Manualidades',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name: 'Mascotas',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Negocios',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Para toda la familia',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name: 'Salud',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Tecnologia',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'Viajes',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:'+18',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:  'Beneficiencia',
+        description: 'Duis autem vel eum iriure'
+    },
+    {
+        name:   'Otros',
+        description: 'Duis autem vel eum iriure'
+    },
+]
