34a9671fa93f5f6befed6856e58d861a39d4ce44
unknown <jeanpier.dev@outlook.com>
2022-09-12 09:51:40 -0500

    css formateado

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <div className="App">Hola mundo</div>;
 }
 
 export default App;
