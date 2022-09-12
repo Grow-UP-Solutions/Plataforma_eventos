c72a8867363362c5fd30376003b248b3c4aa88f3
lschacon1988 <lschacon1988@gmail.com>
2022-09-12 14:18:56 -0500

    Api con express
    Cambio la configuracion de la api a express

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hola mundo</div>;
+  return <div className="App"><h1>Hola mundo</h1></div>;
 }
 
 export default App;
