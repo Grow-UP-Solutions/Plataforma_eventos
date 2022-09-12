42605cad30cefccc8b48252124ca3de8aab6d5f0
gastonluchini <95881992+gastonluchini@users.noreply.github.com>
2022-09-12 17:21:23 -0300

    add: components navbar, home, carrousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Footer />
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../Header/Navbar';
+import CarrouselHome from '../CarrouselHome/CarrouselHome';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+
+  return (
+    <div>
+      <Navbar />
+      <CarrouselHome />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
