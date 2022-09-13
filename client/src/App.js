2b9c5669c8960691d79235c877158328846800b1
unknown <jeanpier.dev@outlook.com>
2022-09-12 19:57:14 -0500

    ADD Page: Contactanos

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home/Home';
+import Contacto from './pages/Contacto/Contacto';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Contacto />
     </div>
   );
 }
--- client/src/pages/Contacto/Contacto.jsx
+++ client/src/pages/Contacto/Contacto.jsx
@@ -9,50 +9,67 @@ const Contacto = () => {
     <div className={styles.pageContacto}>
       <h1 className={styles.contactoTitle}>Contáctanos</h1>
       <address className={styles.contactoAddress}>
-        <div>
-          <img src={pin} alt="pin-icon" />
+        <div className={styles.addressItem}>
+          <img className={styles.addressIcon} src={pin} alt="pin-icon" />
           <p>Colombia</p>
         </div>
-        <div>
-          <img src={phone} alt="phone-icon" />
+        <div className={styles.addressItem}>
+          <img className={styles.addressIcon} src={phone} alt="phone-icon" />
           <p>345 678 98 56</p>
         </div>
-        <div>
-          <img src={mail} alt="mail-icon" />
+        <div className={styles.addressItem}>
+          <img className={styles.addressIcon} src={mail} alt="mail-icon" />
           <p>info@loquequierohacer.com</p>
         </div>
       </address>
       <div className={styles.containerForm}>
-        <p>
-          ¿Ya visitaste nuestra sección de Preguntas frecuentes? En ella podrás
-          encontrar respuesta a muchas de las preguntas más comunes.
+        <p className={styles.info}>
+          ¿Ya visitaste nuestra sección de <a href="#"> Preguntas frecuentes</a>
+          ? En ella podrás encontrar respuesta a muchas de las preguntas más
+          comunes.
         </p>
 
         <form action="">
-          <div>
+          <div className={styles.inputContainer}>
             <label htmlFor="name">Nombre completo:</label>
-            <input type="text" id="name" placeholder="Carla García Montoya" />
+            <input
+              type="text"
+              id="name"
+              placeholder="Carla García Montoya"
+              required
+            />
           </div>
-          <div>
+          <div className={styles.inputContainer}>
             <label htmlFor="mail">Email:</label>
-            <input type="text" id="mail" placeholder="email@ejemplo.com" />
+            <input
+              type="text"
+              id="mail"
+              placeholder="email@ejemplo.com"
+              required
+            />
           </div>
-          <div>
+          <div className={styles.inputContainer}>
             <label htmlFor="city">Ciudad:</label>
-            <input type="text" id="city" />
+            <input type="text" id="city" required />
           </div>
-          <div>
-            <label htmlFor="phone">Celular:</label>
+          <div className={styles.inputContainer}>
+            <label htmlFor="phone">Celular (opcional):</label>
             <input type="text" id="phone" />
           </div>
-          <div>
+          <div className={styles.inputContainer}>
             <label htmlFor="message">Mensaje:</label>
             <textarea
               name="message"
               id="message"
               cols="30"
               rows="10"
+              required
             ></textarea>
+          </div>
+          <div className={styles.containerBtn}>
+            <button className={styles.btnForm} type="submit">
+              Enviar
+            </button>
           </div>
         </form>
       </div>
