e171dfa01a1eea9226baed4822f434f9eb81eef2
guillobr <guillermobr88@gmail.com>
2022-09-14 17:16:08 -0300

    comment

--- client/src/components/EventDetails/EventComments.jsx
+++ client/src/components/EventDetails/EventComments.jsx
@@ -1,39 +1,49 @@
 import React from "react";
+import styles from './EventComments.module.css';
+import ReportProblemIcon from '@mui/icons-material/ReportProblem';
 
 
 
 
 const EventComments =  ({event}) => {
 
-   
+
+
     return (
-      <div >
-        <p>Opiniones del Evento</p>
-        <p>{event.opinions.length}</p>
-        <span>{event.rating}</span>
+      <div className={styles.container} >
+        <p className={styles.title}>Opiniones del Evento</p>
+        <p className={styles.ratNumber}>{event.opinions.length} opiniones - </p>
+        <span className={styles.ratProm}>{event.rating} Positivas</span>
        {event.opinions.length > 0 ? 
          event.opinions.map( opinion => 
-            <div>
-                <p>{opinion.user}</p>
-                <p>{opinion.rating}</p>
-                <p>{opinion.time}</p>
+            <div className={styles.comment}>
+              <div className={styles.userComment}>
                 <img src={opinion.picture} alt="Not Found ):" width="20px" height="30px"/>
-                <p>{opinion.opinion}</p>
-                <p>Reportar contenido inapropiado</p>
+                <div  className={styles.infoComment}>
+                  <span  className={styles.user}>{opinion.user}</span>
+                  <span  className={styles.rating}>{opinion.rating}</span>
+                  <p  className={styles.time}>{opinion.time}</p>
+                  <p  className={styles.opinion}>{opinion.opinion}</p>
+                </div>
+                  <ReportProblemIcon></ReportProblemIcon>
+                </div>
+                <hr></hr>
             </div>)
          
        : 'No hay comentarios'}
 
-        <textarea
+        <textarea 
+            className={styles.textarea}
             type='text'
             placeholder="Escribe un Comentario"
           />
          <input
+            className={styles.rate}
             type='numbre'
             placeholder="Rate"
             />
         
-        <button>Enviar</button>
+        <button  className={styles.button}>Enviar</button>
       </div>
     );
   };
