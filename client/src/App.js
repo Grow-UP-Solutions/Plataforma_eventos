6c6f15c75b36fdbc33b8b0f4facd7e375ed0ca8b
unknown <jeanpier.dev@outlook.com>
2022-09-14 08:25:06 -0500

    FIX: Scroll to top

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import Contacto from './pages/Contacto/Contacto';
@@ -8,6 +8,11 @@ import Navbar from './components/Header/Navbar';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <div className="App">
       <Navbar />
