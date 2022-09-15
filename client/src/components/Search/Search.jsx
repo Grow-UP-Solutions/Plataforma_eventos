42bc856fd374322024fe6ab4ae7e6923d9ada7dd
unknown <jeanpier.dev@outlook.com>
2022-09-15 09:02:25 -0500

    FIX: HERO

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import style from './Search.module.css';
 
-const Search = () => {
+import { BsSearch } from 'react-icons/bs';
 
+const Search = () => {
   return (
     <div className={style.container}>
-      <input className={style.input} type="text" placeholder='Buscar'/>
-
+      <input className={style.input} type="text" placeholder="Buscar" />
+      <button className={style.searchBtn}>
+        <BsSearch className={style.iconSearch} />
+      </button>
     </div>
   );
-}
+};
 
 export default Search;
