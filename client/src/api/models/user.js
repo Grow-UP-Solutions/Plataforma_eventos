861f5de6481217f2398fceedcdbce450bcc4613e
guillobr <guillermobr88@gmail.com>
2022-09-22 09:25:16 -0300

    m

---
+++ client/src/api/models/user.js
@@ -0,0 +1,44 @@
+user=[{
+    id: '',
+    name: '',
+    surname:'',
+    nickname:'',
+    address: '',
+    city: '',
+    email: '',
+    phone:'',
+    celPhone:'',
+    documentNº: '',
+    password:'',
+    dniPicutreFront:'',
+    dniPicutreBack:'',
+    aboutMe: '',
+    myEventsCreated: [], 
+    myEventsSaved:[],
+    myEventsPendings:[],
+    myEventsBooked:[],
+    myFavourites:[],
+    bankAccount:[],
+    promotionsAlerts:'',
+    myNextEventsAlerts:'',
+    referalCode:'',
+    facturacion:[],
+    taxes:'',
+    messages:[],
+    opinionsAboutOrg:[],
+    rating: 5,
+    membership: '',
+    picture: '',
+    isSuperAdmin: true,
+    isAdmin: true,
+    isOrganizer: true,
+    isLogged: true,  
+}]
+
+
+/*Un modelo nuevo para referidos
+Codigo
+Valor
+Redimido por
+Fecha
+*/
