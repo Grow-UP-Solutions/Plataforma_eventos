ee7f716f1a4ec4e312b3734918cb8d33367a0e83
unknown <jeanpier.dev@outlook.com>
2022-10-07 17:41:52 -0500

    Fix: Code Verify

--- api/src/models/util/functionDB/CodeVerifyEmailDb.js
+++ api/src/models/util/functionDB/CodeVerifyEmailDb.js
@@ -1,18 +1,18 @@
 import '../../../DB.js';
-import CodeVerifyMail from '../../db/CodeVerifyMail.js';
+import CodeVerifyEmail from '../../db/CodeVerifyEmail.js';
 
 /** basic user database operations */
 
 /**Creating Category in Database */
 export async function createCodeVerifyMail(code) {
-  const codeCreated = new CodeVerifyMail({ code });
+  const codeCreated = new CodeVerifyEmail({ code });
   return await codeCreated.save();
 }
 
 export async function deleteCodeVerifyMail(id) {
-  return await CodeVerifyMail.findByIdAndDelete({ _id: id }).lean();
+  return await CodeVerifyEmail.findByIdAndDelete({ _id: id }).lean();
 }
 
 export async function getCodeVerifyEmail(code) {
-  return await CodeVerifyMail.findOne({ code }).lean();
+  return await CodeVerifyEmail.findOne({ code }).lean();
 }
