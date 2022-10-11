7f0f6703747ce671ee68c84049a95587d949a34f
lschacon1988 <lschacon1988@gmail.com>
2022-10-11 09:17:38 -0500

    update model Events

---
+++ api/src/models/util/functionDB/CodeVerifyMailDb.js
@@ -0,0 +1,19 @@
+import '../../../DB.js';
+import CodeVerifyMail from '../../db/CodeVerifyMail.js';
+
+/** basic user database operations */
+
+/**Creating Category in Database */
+export async function createCodeVerifyMail(code) {
+  const codeCreated = new CodeVerifyMail({ code });
+  return await codeCreated.save();
+}
+
+
+export async function deleteCodeVerifyMail(id) {
+  return await CodeVerifyMail.findByIdAndDelete({ _id: id }).lean();
+}
+
+export async function getCodeVerifyEmail(code) {
+  return await CodeVerifyMail.findOne({ code }).lean();
+}
