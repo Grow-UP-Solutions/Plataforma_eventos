a342f0cd39fa2ed2ec222baee1a731a3867d2a6b
guillobr <guillermobr88@gmail.com>
2022-10-12 18:38:16 -0300

    form fixes

--- client/src/components/CardComments/CardComments.jsx
+++ client/src/components/CardComments/CardComments.jsx
@@ -4,13 +4,13 @@ import styles from './CardComments.module.css';
 import avatar from '../../assets/imgs/no-avatar.png';
 import ReportProblemIcon from '@mui/icons-material/ReportProblem';
 import { Rating } from '@mui/material';
-import { format } from "timeago.js";
+// import { format } from "timeago.js";
 
 const CardComments = ({ o }) => {
 
   return (
     <div className={styles.userComment}>
-      <div>
+      {/* <div>
         <img className={styles.picture} src={avatar} alt="Not Found ):" width="20px" height="30px"/>
       </div>
 
@@ -32,7 +32,7 @@ const CardComments = ({ o }) => {
         <div className={styles.reportCom} data-hover='Reportar contenido inapropiado'>
           <ReportProblemIcon sx={{ fontSize: '40px', color: '#cbcbcb'  }}/>
         </div>
-      </div>
+      </div> */}
     </div>           
   );
 }
