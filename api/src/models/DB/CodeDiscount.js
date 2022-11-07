a68c222705f41e99d82753b20b0e57ea3ad7565f
Jean <jeanpier.dev@outlook.com>
2022-11-07 08:45:41 -0500

    codigo de descuento Event

--- api/src/models/DB/CodeDiscount.js
+++ api/src/models/DB/CodeDiscount.js
@@ -5,10 +5,6 @@ const codeDiscountSchema = new Schema({
     type: String,
     default: '',
   },
-  idEvent: {
-    type: String,
-    default: '',
-  },
   code: {
     type: String,
     default: '',
@@ -22,18 +18,6 @@ const codeDiscountSchema = new Schema({
     default: false,
   },
   dateRedimeed: {
-    type: String,
-    default: '',
-  },
-  quotas: {
-    type: Number,
-    default: 1,
-  },
-  percentage: {
-    type: Number,
-    default: 0,
-  },
-  dateEvent: {
     type: String,
     default: '',
   },
