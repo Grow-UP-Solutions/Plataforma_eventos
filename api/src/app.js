9ef5cf2160eb5c40a99ad45b6991328cd74d39ed
Jean <jeanpier.dev@outlook.com>
2022-11-10 10:18:52 -0500

    fix test open graph

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,3 @@
-//@ts-check
 const express = require('express');
 const prerender = require('prerender-node');
 const bodyParser = require('body-parser');
@@ -11,11 +10,13 @@ require('./models/util/middlewares/facebook-auth.js');
 require('./models/util/middlewares/google-auth.js');
 
 const server = express();
-server.use(prerender).set('prerenderToken', '5hdauSmv64TUxC0CvXWW');
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cors());
 server.use('/', router);
 server.use(morgan('dev'));
 server.use(passport.initialize());
+server.use(prerender.set('prerenderToken', '5hdauSmv64TUxC0CvXWW'));
+
 module.exports = server;
