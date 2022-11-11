e44eb1b5400b9dfeee7e7facd8f36a8164c8cfb0
Jean <jeanpier.dev@outlook.com>
2022-11-11 08:07:28 -0500

    fix conflicts

--- api/src/models/util/middlewares/facebook-auth.js
+++ api/src/models/util/middlewares/facebook-auth.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: 'https://plataformaeventos-production-6111.up.railway.app/users/login/facebook/callback',
+      callbackURL: 'https://plataformaeventos-production.up.railway.app/users/login/facebook/callback',
       profileFields: ['displayName', 'email', 'photos'],
     },
     function (accessToken, refreshToken, profile, done) {
