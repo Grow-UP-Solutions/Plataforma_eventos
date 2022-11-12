1147a84362b8232630db680e265a86525b17a6f7
Jean <jeanpier.dev@outlook.com>
2022-11-12 12:26:07 -0500

    fix favorites

--- api/src/models/util/functionDB/users/myFavorites.js
+++ api/src/models/util/functionDB/users/myFavorites.js
@@ -1,18 +1,19 @@
-const EventFunctionDb = require("../event/index.event");
-const UsersFunctionDb = require("./index.users");
+const { getUser } = require('../../../../routes/services/users.services');
+const EventFunctionDb = require('../event/index.event');
+const UsersFunctionDb = require('./index.users');
 
 module.exports = async function updateMyFavorites(idUser, idEvent) {
-   try {
-      const user = await UsersFunctionDb.oneUser(idUser);
+  try {
+    const user = await getUser(idUser);
 
-      const event = await EventFunctionDb.oneEvent(idEvent);
-      
-      user.myFavorites.push(event._id);
-      
-      await user.save();
+    const event = await EventFunctionDb.oneEvent(idEvent);
 
-      return event;
-   } catch (error) {
-      throw new Error(error.message);
-   }
+    user.myFavorites.push(event._id);
+
+    await user.save();
+
+    return event;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
