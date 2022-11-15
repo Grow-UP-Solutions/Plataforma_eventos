4575bd8d740e4d6aa5bfb04c34c01e8f99d0a4d5
lschacon1988 <lschacon1988@gmail.com>
2022-11-14 21:54:21 -0500

    pasarela al 70%

--- api/src/routes/controller/pasarelaPagoController/paymentController.js
+++ api/src/routes/controller/pasarelaPagoController/paymentController.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Router } = require("express");
 const router = Router();
 const mercadopago = require("mercadopago");
+const UsersFunctionDb = require("../../../models/util/functionDB/users/index.users");
 const { ACCESS_TOKEN } = process.env;
 
 mercadopago.configure({
@@ -10,47 +11,55 @@ mercadopago.configure({
 
 router.post("/pago", async (req, res) => {
    const carrito = req.body;
-   const {codigoDescuento}= req.query
-   
-   
+   const { codigoDescuento } = req.query;
 
-   const monto = carrito?.map((e) => {
-         const montoTem = e.unit_price * e.quantity;
-         return montoTem;
-      })
-      .reduce((a, b) => a + b);
+   // const monto = carrito?.map((e) => {
+   //       const montoTem = e.unit_price * e.quantity;
+   //       return montoTem;
+   //    })
+   //    .reduce((a, b) => a + b);
 
-   const userDB = await Users.findOne({ email: email[0] });
+   const userDB = await UsersFunctionDb.oneUser(carrito.idUser);
+   const telefono = userDB.tel.split(" ").join("");
+   console.log(parseInt(telefono));
+   console.log("Carrito", carrito);
+   //console.log("USERS", userDB);
 
-   const newOrder = new Orders({
-      status: EnumStatus.PENDING,
-      fecha: new Date(),
-      usuario: userDB._id,
-      produt: carrito.map((e) => e.title),
-      total: monto,
-      payment_id: 0,
-      status_order: Enum.CREATED,
-      libros: carrito.map((e) => {
-         return { title: e.title, quantity: e.quantity };
-      }),
-   });
+   // const newOrder = new Orders({
+   //    status: EnumStatus.PENDING,
+   //    fecha: new Date(),
+   //    usuario: userDB._id,
+   //    produt: carrito.map((e) => e.title),
+   //    total: monto,
+   //    payment_id: 0,
+   //    status_order: Enum.CREATED,
+   //    libros: carrito.map((e) => {
+   //       return { title: e.title, quantity: e.quantity };
+   //    }),
+   // });
 
-   await newOrder.save();
-   userDB.buyBooks = userDB.buyBooks.concat(newOrder._id);
-   await userDB.save();
+   //await newOrder.save();
+   // userDB.buyBooks = userDB.buyBooks.concat(newOrder._id);
+   // await userDB.save();
 
    try {
-      const itemsMp = carrito?.map((e) => ({
-         title: e.title,
-         unit_price: Number(e.unit_price),
-         quantity: Number(e.quantity),
-      }));
+      const itemsMp = carrito;
 
       let preference = {
-         items: itemsMp,
+         items: [itemsMp],
          nameUser: userDB.name,
          emailUser: userDB.email,
-         external_reference: `${newOrder._id}`,
+         external_reference: `${userDB._id}`,
+         payer: {
+            name: userDB.name,
+            surname: "user-surname",
+            email: userDB.email,
+            date_created: Date.now,
+            phone: {
+               area_code: "57",
+               number: parseInt(telefono),
+            },
+         },
          payment_methods: {
             excluded_payment_type: [
                {
@@ -61,23 +70,60 @@ router.post("/pago", async (req, res) => {
          },
 
          back_urls: {
-            success: "https://e-commerce-books.vercel.app/mercadopago/success",
-            failure: "https://e-commerce-books.vercel.app/mercadopago/success",
-            pending: "https://e-commerce-books.vercel.app/mercadopago/success",
+            success: "http://localhost:3001/pago/success",
+            failure: "http://localhost:3001/pago/fail",
+            pending: "http://localhost:3001/pago/pending",
          },
          auto_return: "approved",
+         taxes: [
+            {
+               type: "IVA",
+               value: 19,
+            },
+         ],
       };
-      const saveOrder = await Orders.findById({ _id: newOrder._id }).populate({
-         path: "usuario",
-      });
+     
 
       const respuesta = await mercadopago.preferences.create(preference);
 
       const globalInitPoint = respuesta.body.init_point;
-      return res.json({ init_point: globalInitPoint, order: saveOrder });
+      return res.json({ init_point: globalInitPoint });
    } catch (error) {
-      return console.log("FALLO MERCADO PAGO", error);
+      res.status(500).json(error.message);
    }
 });
 
-module.exports= router
+router.get('/success', async (req, res) => {
+   const algo = req.query
+   try {
+    console.log('/*/*/*//*/',algo)
+     res.json(algo)
+     
+   } catch (error) {
+     return res.json({ msg: 'FALLO SUCCESS ', error: error })
+   }
+ })
+
+ router.get('/fail', async (req, res) => {
+   const algo = req.query
+   try {
+    console.log('/*/*/*//*/',algo)
+     res.json(algo)
+     
+   } catch (error) {
+     return res.json({ msg: 'FALLO SUCCESS ', error: error })
+   }
+ })
+
+ router.get('/pending', async (req, res) => {
+   const algo = req.query
+   try {
+    console.log('/*/*/*//*/',algo)
+     res.json(algo)
+     
+   } catch (error) {
+     return res.json({ msg: 'FALLO SUCCESS ', error: error })
+   }
+ })
+
+module.exports = router;
