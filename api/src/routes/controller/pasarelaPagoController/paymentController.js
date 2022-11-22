0c984d4316c3acb9b1de48766d6de662750cc267
lschacon1988 <lschacon1988@gmail.com>
2022-11-22 07:30:48 -0500

    update Pasarela

--- api/src/routes/controller/pasarelaPagoController/paymentController.js
+++ api/src/routes/controller/pasarelaPagoController/paymentController.js
@@ -13,8 +13,9 @@ mercadopago.configure({
 
 router.post("/orden", async (req, res) => {
    const { dates, idUser, idEvent } = req.body;
-   const { codigo } = dates;
-
+   
+   const codigosPrueba = dates.map(e => e.codigo)
+   console.log(codigosPrueba)
    const userDB = await UsersFunctionDb.oneUser(idUser);
 
    const eventDB = await EventFunctionDb.oneEvent(idEvent);
@@ -60,7 +61,7 @@ router.post("/orden", async (req, res) => {
 
          back_urls: {
             success:
-               "https://plataformaeventos-production.up.railway.app/mercadoPago/success",
+               `http://localhost:3001/mercadoPago/success?codigo=${codigosPrueba}`,
             failure:
                "https://plataformaeventos-production.up.railway.app/mercadoPago/fail",
          },
@@ -85,8 +86,10 @@ router.post("/orden", async (req, res) => {
 });
 
 router.get("/success", async (req, res) => {
-   const { external_reference, payment_id, preference_id } = req.query;
+   const { external_reference, payment_id, preference_id, codigo } = req.query;
+   console.log(req.query)
    const ids = external_reference.split(",");
+
    const idEvent = ids[0];
 
    const idUser = ids[1];
@@ -152,7 +155,17 @@ router.get("/success", async (req, res) => {
          await user.save();
       }
 
-      res.json({ response });
+      const resultTransaccion = {
+         codigoDeLaTransaccion : payment_id,
+         DestinoDePago:response.statement_descriptor,
+         fechaDePago: response.date_created,
+         valorDeLaTransaccion: response.net_amount,
+         costoDeLaTransaccion: response.net_amount,
+          referencia: response.payer.identification.number
+
+      }
+
+      res.json(resultTransaccion);
    } catch (error) {
       console.log(error.message);
       return res.status(500).json(error.message);
@@ -178,3 +191,120 @@ module.exports = router;
 // const dataPreference = await axios(
 //    `https://api.mercadopago.com/checkout/preferences/${preference_id}?access_token=${ACCESS_TOKEN}`
 // );
+
+
+
+const algo={
+   "id": 9999999999,
+   "status": "closed",
+   "external_reference": "default",
+   "preference_id": "Preference identification",
+   "payments": [
+     {
+       "id": 9999999999,
+       "transaction_amount": 1,
+       "total_paid_amount": 1,
+       "shipping_cost": 0,
+       "currency_id": "BRL",
+       "status": "approved",
+       "status_detail": "accredited",
+       "date_approved": "2019-04-02T18:35:35.000Z",
+       "date_created": "2019-04-02T18:35:34.000Z",
+       "last_modified": "2019-04-02T18:35:35.000Z",
+       "amount_refunded": 0
+     }
+   ],
+   "shipments": [
+     {
+       "id": 99999999999,
+       "shipment_type": "shipping",
+       "shipping_mode": "me2",
+       "status": "delivered",
+       "items": [
+         {
+           "id": "not specified",
+           "description": "shipment item description",
+           "quantity": 1,
+           "dimensions": "1.0x10.0x23.0100.0"
+         }
+       ],
+       "date_created": "2019-04-02T18:20:46.000Z",
+       "last_modified": "2019-04-12T19:36:48.000Z",
+       "date_first_printed": "2019-04-02T18:35:40.000Z",
+       "service_id": 999,
+       "sender_id": 999999999,
+       "receiver_id": 999999999,
+       "receiver_address": {
+         "id": 9999999999,
+         "address_line": "address line",
+         "city": {
+           "name": "City Name"
+         },
+         "state": {
+           "id": "AR-X",
+           "name": "state name"
+         },
+         "country": {
+           "id": "AR",
+           "name": "Argentina"
+         },
+         "latitude": 0,
+         "longitude": 0,
+         "comment": "shipment comment",
+         "contact": "shipment contact",
+         "phone": 9999999999,
+         "zip_code": 9999,
+         "street_name": "Nombre de la calle",
+         "street_number": 999
+       },
+       "shipping_option": {
+         "id": 999999999,
+         "cost": 0,
+         "currency_id": "BRL",
+         "shipping_method_id": 99999,
+         "estimated_delivery": {
+           "date": "2019-04-11T03:00:00.000Z"
+         },
+         "name": "Normal a domicilio",
+         "list_cost": 0,
+         "speed": {
+           "handling": 72,
+           "shipping": 72
+         }
+       }
+     }
+   ],
+   "payouts": {},
+   "collector": {
+     "id": 999999999,
+     "email": "vendedor+329653108@adf12.com.br",
+     "nickname": "TESTRPEHE21Q"
+   },
+   "marketplace": "NONE",
+   "date_created": "2018-09-14T17:11:31.000Z",
+   "last_updated": "2018-09-14T17:11:43.000Z",
+   "shipping_cost": 0,
+   "total_amount": 5,
+   "site_id": "mla",
+   "paid_amount": 5,
+   "refunded_amount": 0,
+   "payer": {
+     "id": 999999999
+   },
+   "items": [
+     {
+       "id": "item id",
+       "category_id": "item category",
+       "currency_id": "BRL",
+       "description": "item description",
+       "picture_url": "item picture url",
+       "title": "item title",
+       "quantity": 1,
+       "unit_price": 5
+     }
+   ],
+   "cancelled": false,
+   "additional_info": "additional information",
+   "application_id": 10000000000000000,
+   "order_status": "paid"
+ }
