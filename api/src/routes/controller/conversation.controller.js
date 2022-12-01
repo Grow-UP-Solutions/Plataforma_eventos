a2bea8fa82aa5a1517cf36675001432a7acf321f
JeanPierHG <jeanpier.dev@outlook.com>
2022-12-01 18:13:50 -0500

    add route: delete conversation

--- api/src/routes/controller/conversation.controller.js
+++ api/src/routes/controller/conversation.controller.js
@@ -8,14 +8,13 @@ const {
   pinupConversation,
   createConversationAllBuyer,
 } = require('../../models/util/functionDB/ConversationDb.js');
+const MessageFunctionDb = require('../../models/util/functionDB/message/index.message.js');
 
 const router = Router();
 
-
-
 router.get('/:userId', async (req, res) => {
   const { userId } = req.params;
-  console.log(userId)
+  console.log(userId);
   try {
     const allConversation = await findConversation(userId);
     res.status(200).json(allConversation);
@@ -36,6 +35,19 @@ router.post('/create', async (req, res) => {
   }
 });
 
+router.delete('/delete', async (req, res) => {
+  const { idLastConversation } = req.body;
+
+  try {
+    const messages = await MessageFunctionDb.findMessage(idLastConversation);
+
+    if (messages.length > 0) return res.json({ message: 'No eliminado' });
+    await deleteConversation(idLastConversation);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
 router.post('/buyer/create', async (req, res) => {
   const menbers = req.body;
 
@@ -48,24 +60,24 @@ router.post('/buyer/create', async (req, res) => {
   }
 });
 
-router.put('/:idConversation', async (req, res)=>{
-const {idConversation} = req.params
+router.put('/:idConversation', async (req, res) => {
+  const { idConversation } = req.params;
   try {
-    const lockConversation = await lockedConversation(idConversation)
-    return res.status(200).json(lockConversation)
+    const lockConversation = await lockedConversation(idConversation);
+    return res.status(200).json(lockConversation);
   } catch (error) {
     return res.status(500).json({ faill: error.message });
   }
-})
-router.put('/:idConversation/pinup', async (req, res)=>{
-const {idConversation} = req.params
-console.log(idConversation)
+});
+router.put('/:idConversation/pinup', async (req, res) => {
+  const { idConversation } = req.params;
+  console.log(idConversation);
   try {
-    const pinupConversations = await pinupConversation(idConversation)
-    return res.status(200).json(pinupConversations)
+    const pinupConversations = await pinupConversation(idConversation);
+    return res.status(200).json(pinupConversations);
   } catch (error) {
     return res.status(500).json({ faill: error.message });
   }
-})
+});
 
 module.exports = router;
