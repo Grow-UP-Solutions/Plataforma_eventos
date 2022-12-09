c9c2c34c216cec9e9cfc999bbc90861c497ff657
guillobr <guillermobr88@gmail.com>
2022-12-09 15:57:44 -0300

    fixes varios

--- client/src/components/Admin/OrganizerBills.jsx
+++ client/src/components/Admin/OrganizerBills.jsx
@@ -8,6 +8,7 @@ import eventsApi from '../../axios/eventsApi';
 import { AiOutlineConsoleSql } from 'react-icons/ai';
 import { fechaActual } from '../../utils/fechaActual';
 import { Link } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 const OrganizerBills = () => {
   const [state, fetchUsers] = useFetch();
@@ -15,6 +16,7 @@ const OrganizerBills = () => {
   const id = useParams().id;
   const [userData, setUserData] = useState({});
   const [billNumber, setBillNumber] = useState();
+  const [load, setLoad] = useState(true);
 
   useEffect(() => {
     getUserData();
@@ -24,6 +26,7 @@ const OrganizerBills = () => {
     if (id) {
       const userResult = await eventsApi.get(`/users/${id}`);
       setUserData(userResult.data);
+      setLoad(false)
     }
   };
 
@@ -33,18 +36,9 @@ const OrganizerBills = () => {
   const billsPerPage = 6;
   const indexOfLastBill = currentPage * billsPerPage;
   const indexOfFirstBill = indexOfLastBill - billsPerPage;
-  //const currentBill = userData.factura.slice(indexOfFirstBill, indexOfLastBill);
   const paginado = (pageNumber) => setCurretPage(pageNumber);
 
-  useEffect(
-    function() {
-      fetchUsers({
-        url: 'https://reqres.in/api/users',
-        method: 'GET',
-      });
-    },
-    [fetchUsers]
-  );
+ 
 
   function handleChange(e) {
     e.preventDefault();
@@ -73,15 +67,13 @@ const OrganizerBills = () => {
     }
   };
 
-  if (state.isLoading) {
-    return <div>Cargando usuarios...</div>;
-  }
 
-  if (state.isFailed) {
-    return <div>Fallo recuperando los usuarios</div>;
-  }
 
-  if (state.isSuccess) {
+  if(load){
+    return(
+      <Loading />
+    )
+   }else{
     return (
       <div className={style.container}>
         <div className={style.container_titles}>
@@ -102,27 +94,6 @@ const OrganizerBills = () => {
               </tr>
             </thead>
 
-            {/* <tbody>
-              {
-                
-                state.data.data.slice(indexOfFirstBills, indexOfLastBills).map((e) => (
-                  <tr key={e.id} className={style.tbody}>
-                    <td className={style.tbody_name}>
-                      <img src={userData.userpicture} alt={e.first_name} 
-                        style={{maxWidth: '20%', borderRadius: '100px'}}
-                      />
-                      <p>Hiking with my dog</p> 
-                    </td>
-                    <td>{e.email}</td>
-                    <td>xxxxxxx</td>
-                    <td>xxxxxxx</td>
-                    <td>$500.000</td>
-                    <td><input type="checkbox" /></td>
-                  </tr>
-                ))
-              }
-            </tbody> */}
-
             <tbody>
               {userData.myEventsCreated !== undefined &&
                 userData.myEventsCreated.slice(indexOfFirstBill, indexOfLastBill).map((event) =>
@@ -142,7 +113,6 @@ const OrganizerBills = () => {
                         <td>
                           <input
                             type='text'
-                            placeholder='Numero de factura'
                             name='billNumber'
                             value={billNumber}
                             onChange={(e) => handleChange(e)}
@@ -151,7 +121,7 @@ const OrganizerBills = () => {
                         </td>
                         <td>{date.overallEarnings}</td>
                         <td>
-                          <button onClick={(e) => pagar(e, event._id, date._id, date.overallEarnings)}>Pagar</button>
+                          <button className={style.pagar} onClick={(e) => pagar(e, event._id, date._id, date.overallEarnings)}>Pagar</button>
                         </td>
                       </tr>
                     ) : (
@@ -180,7 +150,7 @@ const OrganizerBills = () => {
           )}
 
           <div className={style.container_exit}>
-            <p className={style.exit} onClick={() => navigate('/user/profile')}>
+            <p className={style.exit} onClick={() => navigate('/lista-de-organizadores')}>
               Salir
             </p>
           </div>
@@ -188,7 +158,7 @@ const OrganizerBills = () => {
       </div>
     );
   }
-  return null;
+
 };
 
 export default OrganizerBills;
--- client/src/components/Finance/Bills.jsx
+++ client/src/components/Finance/Bills.jsx
@@ -7,12 +7,14 @@ import { useNavigate, useParams } from 'react-router-dom';
 import eventsApi from '../../axios/eventsApi';
 import { AiOutlineConsoleSql } from 'react-icons/ai';
 import { fechaActual } from '../../utils/fechaActual';
+import Loading from '../Loading/Loading';
 
 const Bills = () => {
   const [state, fetchUsers] = useFetch();
   const navigate = useNavigate();
   const id = useParams().id;
   const [userData, setUserData] = useState({});
+  const [load, setLoad] = useState(true);
 
   useEffect(() => {
     getUserData();
@@ -23,6 +25,7 @@ const Bills = () => {
       const userResult = await eventsApi.get(`/users/${id}`);
       setUserData(userResult.data);
     }
+    setLoad(false)
   };
 
   console.log('userData:', userData);
@@ -31,150 +34,91 @@ const Bills = () => {
   const billsPerPage = 6;
   const indexOfLastBill = currentPage * billsPerPage;
   const indexOfFirstBill = indexOfLastBill - billsPerPage;
-  //const currentBill = userData.factura.slice(indexOfFirstBill, indexOfLastBill);
   const paginado = (pageNumber) => setCurretPage(pageNumber);
 
-  // const facturas = userData.factura
+  
 
-  // const orderByDate = facturas.sort((a, b) => {
-  //   if (a.fechaDeFacturacion < b.fechaDeFacturacion) return -1;
-  //   if (b.fechaDeFacturacion < a.fechaDeFacturacion) return 1;
-  //   return 0;
-  // });
+ if(load){
+  return(
+    <Loading />
+  )
+ }else{
+  return (
+    <div className={style.container}>
+      <div className={style.container_titles}>
+        <h1>Histórico de facturas</h1>
+        <h3>{userData.name}</h3>
+        <h5>{fechaActual}</h5>
+      </div>
 
-  // useEffect(() => {
-  //   console.log('userData en effect:',userData)
-  //   const orderByDate = userData.factura.sort((a, b) => {
-  //     if (a.fechaDeFacturacion < b.fechaDeFacturacion) return -1;
-  //     if (b.fechaDeFacturacion < a.fechaDeFacturacion) return 1;
-  //     return 0;
-  //   });
+      <div className={style.container_table}>
+        <div className={style.container_headbody}>
+          <thead className={style.thead}>
+            <tr className={style.tr}>
+              <th className={style.th_first}>Nombre del evento</th>
+              <th>Fecha del evento</th>
+              <th>Fecha de facturacion</th>
+              <th>Número de factura</th>
+              <th>Tu ganancia</th>
+            </tr>
+          </thead>
 
-  // console.log('orderByDate:',orderByDate)
-
-  // }, [userData]);
-
-  useEffect(
-    function() {
-      fetchUsers({
-        url: 'https://reqres.in/api/users',
-        method: 'GET',
-      });
-    },
-    [fetchUsers]
-  );
-
-  if (state.isLoading) {
-    return <div>Cargando usuarios...</div>;
-  }
-
-  if (state.isFailed) {
-    return <div>Fallo recuperando los usuarios</div>;
-  }
-
-  if (state.isSuccess) {
-    return (
-      <div className={style.container}>
-        <div className={style.container_titles}>
-          <h1>Histórico de facturas</h1>
-          <h3>{userData.name}</h3>
-          <h5>{fechaActual}</h5>
+          <tbody>
+            {userData.myEventsCreated !== undefined &&
+              userData.myEventsCreated.slice(indexOfFirstBill, indexOfLastBill).map((event) =>
+                event.dates.map((date) =>
+                  date.sells > 0 ? (
+                    <tr key={event._id} className={style.tbody}>
+                      <td className={style.tbody_name}>
+                        <img
+                          src={event.pictures[0].picture}
+                          alt={''}
+                          style={{ maxWidth: '20%', borderRadius: '100px' }}
+                        />
+                        <p>{event.title}</p>
+                      </td>
+                      <td>{date.date}</td>
+                      <td>{date.isPay === false ? 'PENDIENTE' : date.datePay}</td>
+                      <td>{date.isPay === false ? 'PENDIENTE' : date.billNumber}</td>
+                      <td>{date.overallEarnings}</td>
+                      <td>
+                        <input type='checkbox' />
+                      </td>
+                    </tr>
+                  ) : (
+                    ''
+                  )
+                )
+              )}
+          </tbody>
         </div>
 
-        <div className={style.container_table}>
-          <div className={style.container_headbody}>
-            <thead className={style.thead}>
-              <tr className={style.tr}>
-                <th className={style.th_first}>Nombre del evento</th>
-                <th>Fecha del evento</th>
-                <th>Fecha de facturacion</th>
-                <th>Número de factura</th>
-                <th>Tu ganancia</th>
-              </tr>
-            </thead>
-
-            {/* <tbody>
-              {
-                
-                state.data.data.slice(indexOfFirstBills, indexOfLastBills).map((e) => (
-                  <tr key={e.id} className={style.tbody}>
-                    <td className={style.tbody_name}>
-                      <img src={userData.userpicture} alt={e.first_name} 
-                        style={{maxWidth: '20%', borderRadius: '100px'}}
-                      />
-                      <p>Hiking with my dog</p> 
-                    </td>
-                    <td>{e.email}</td>
-                    <td>xxxxxxx</td>
-                    <td>xxxxxxx</td>
-                    <td>$500.000</td>
-                    <td><input type="checkbox" /></td>
-                  </tr>
-                ))
-              }
-            </tbody> */}
-
-            <tbody>
-              {userData.myEventsCreated !== undefined &&
-                userData.myEventsCreated.slice(indexOfFirstBill, indexOfLastBill).map((event) =>
-                  event.dates.map((date) =>
-                    date.sells > 0 ? (
-                      <tr key={event._id} className={style.tbody}>
-                        <td className={style.tbody_name}>
-                          <img
-                            src={event.pictures[0].picture}
-                            alt={''}
-                            style={{ maxWidth: '20%', borderRadius: '100px' }}
-                          />
-                          <p>{event.title}</p>
-                        </td>
-                        <td>{date.date}</td>
-                        <td>{date.isPay === false ? 'PENDIENTE' : date.datePay}</td>
-                        <td>{date.isPay === false ? 'PENDIENTE' : date.billNumber}</td>
-                        <td>{date.overallEarnings}</td>
-                        {/* <td>{date.date}</td>
-                    <td>PENDIENTE</td>
-                    <td>PENDIENTE'</td>
-                    <td>0</td> */}
-                        <td>
-                          <input type='checkbox' />
-                        </td>
-                      </tr>
-                    ) : (
-                      ''
-                    )
-                  )
-                )}
-            </tbody>
+        <div className={style.container_download}>
+          <div className={style.container_one}>
+            <DescriptionOutlinedIcon fontSize='large' color='#d53e27' />
+            <p>Descargar factura de selecionados (PDF)</p>
           </div>
 
-          <div className={style.container_download}>
-            <div className={style.container_one}>
-              <DescriptionOutlinedIcon fontSize='large' color='#d53e27' />
-              <p>Descargar factura de selecionados (PDF)</p>
-            </div>
-
-            <div className={style.container_two}>
-              <DescriptionOutlinedIcon fontSize='large' />
-              <p>Descargar reporte de páginas (EXCEL)</p>
-            </div>
+          <div className={style.container_two}>
+            <DescriptionOutlinedIcon fontSize='large' />
+            <p>Descargar reporte de páginas (EXCEL)</p>
           </div>
-          {userData.myEventsCreated !== undefined && (
-            <div className={style.container_pagination}>
-              <Pagination billsPerPage={billsPerPage} state={userData.myEventsCreated.length} paginado={paginado} />
-            </div>
-          )}
-
-          <div className={style.container_exit}>
-            <p className={style.exit} onClick={() => navigate('/user/profile')}>
-              Salir
-            </p>
+        </div>
+        {userData.myEventsCreated !== undefined && (
+          <div className={style.container_pagination}>
+            <Pagination billsPerPage={billsPerPage} state={userData.myEventsCreated.length} paginado={paginado} />
           </div>
+        )}
+
+        <div className={style.container_exit}>
+          <p className={style.exit} onClick={() => navigate('/usuario/finanzas')}>
+            Salir
+          </p>
         </div>
       </div>
-    );
-  }
-  return null;
+    </div>
+  );
+ }
 };
 
 export default Bills;
